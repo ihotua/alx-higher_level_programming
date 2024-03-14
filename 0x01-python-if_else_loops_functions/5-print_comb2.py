a9c2f7a2e34b448c4e9fa3b50f68f6cbd21796fb
root <ihotuagbo@gmail.com>
2024-03-13 21:18:43 -0700

    If else

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for num in range(100):
+  print(f"{num:02d}", end=", ")
+
+print(f"{99:02d}")
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for f in range(10):
+    for g in range(f+1, 10):
+        print("{}{},".format(f, g), end="")
+print("99\n")
