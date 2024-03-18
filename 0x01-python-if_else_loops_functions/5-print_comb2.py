6e756ae8a600393bfa2fce8e7f7171dcf7bca86e
root <ihotuagbo@gmail.com>
2024-03-18 12:53:49 -0700

    If else

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num < 99:
-        print("{:02d}".format(num), end=" , ")
-    else:
-        print("{}".format(num))
+    print("{:02d}, ".format(num), end=" ")
+print("99")
