7bc541bd0519d9df1508ceabd9ac64389d5c4a12
root <ihotuagbo@gmail.com>
2024-03-18 13:49:28 -0700

    If else

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ for f in range(10):
     for g in range(f+1, 10):
         if f != 9 or g != 8:
             print("{}{}".format(f, g), end=", ")
-else
-    print("\n")
+        else:
+            print("\n")
