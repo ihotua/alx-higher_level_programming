73edea1701d1999a4433081000c34a3ef7fcd7d7
root <ihotuagbo@gmail.com>
2024-03-21 13:50:59 -0700

    Import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
-        print("No arguments.")
+        print(") arguments.")
     else:
         print("{:d} argument".format(len(sys.argv) - 1), end="")
         if len(sys.argv) > 2:
