8e9e82919f0aa0456f0ac54741da2b01a9fe59a7
root <ihotuagbo@gmail.com>
2024-03-22 16:56:04 -0700

    Data Structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     ihotu = []
-    for i in my_list:
-        ihotu.append(num % 2 == 0)
+    for num in range(len(ihotu)):
+        if ihotu[num] % 2 == 0:
+            ihotu[num] = True
+        else:
+            ihotu[num] = False
     return (ihotu)
