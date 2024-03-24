5e89117f5e9e16a26afdaa1c36b49131f7d555d4
root <ihotuagbo@gmail.com>
2024-03-24 15:33:50 -0700

    Data Structures

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,22 +4,34 @@
 /**
  * print_python_list_info - prints python list info
  *
- * @p: PyObject
+ * @p: PyObject pointer
  * Return: no return
  */
 
 void print_python_list_info(PyObject *p)
 {
-    Py_ssize_t size, i;
+    long int size, i;
     PyObject *item;
 
-    size = PyList_Size(p);
+    if (!PyList_Check(p))
+    {
+        printf("[-] It is not a Python list\n");
+        return;
+    }
+
+    size = Py_SIZE(p);
     printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
     for (i = 0; i < size; i++)
     {
         item = PyList_GetItem(p, i);
+        if (item == NULL)
+        {
+            PyErr_Print();
+            printf("[-] Error: Failed to retrieve list item\n");
+            return;
+        }
+
         printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    } 
+    }
 }
