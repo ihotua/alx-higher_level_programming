d7b38a96763215a9be1d1d110b78c650d35b704e
root <ihotuagbo@gmail.com>
2024-03-26 13:55:39 -0700

    More Data

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     ihotu = sorted(a_dictionary.k())
 
     for k in ihotu:
-        print("{}: {}'.format(k, a_dictionary[k]))
+        print("{}: {}".format(k, a_dictionary[k]))
