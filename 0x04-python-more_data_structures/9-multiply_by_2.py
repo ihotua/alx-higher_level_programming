4882c4e6218604abcd8e9338f2f1414148b4185c
root <ihotuagbo@gmail.com>
2024-03-26 14:51:27 -0700

    More Data

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     ihotu_dict = {}
-    for key in a_dictionary:
+    for k in a_dictionary:
         ihotu_dict[k] = 2 * a_dictionary[k]
     return (ihotu_dict)
