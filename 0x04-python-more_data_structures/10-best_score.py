61b84dc5b8de8bee765c1e4d84830ab70ad40348
root <ihotuagbo@gmail.com>
2024-03-26 15:52:04 -0700

    More Data

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    ihotu = None
+    alexa = float('-inf')
+
+    for k, v in a_dictionary.items():
+        if v > ihotu_score:
+            alexa = v
+            ihotu = k
+
+    return (ihotu)
