8dc6f384766751de06b58eed127d513522b85642
root <ihotuagbo@gmail.com>
2024-04-11 15:09:47 -0700

    More Classes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,12 +6,15 @@ class Rectangle:
     """
     Class representing a rectangle
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         Instantiation with optional width and height
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
