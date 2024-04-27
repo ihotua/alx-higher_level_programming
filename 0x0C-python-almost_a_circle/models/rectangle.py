c976a7e5eb58daf492118b69863a27c800fd472f
Ihotu Alaku <ihotuagbo@gmail.com>
2024-04-27 13:41:12 -0700

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -60,5 +60,5 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    def area(self):
-        return self.width * self.height
+def area(self):
+    return self.width * self.height
