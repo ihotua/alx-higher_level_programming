2f231e64b56d0b9557fe749574eb29cb96bce87f
Ihotu Alaku <ihotuagbo@gmail.com>
2024-04-27 13:23:17 -0700

    Almost a Circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,8 +5,6 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """Class that defines properties of rectangle"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
@@ -16,57 +14,51 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        return (self.__x)
+        return self.__x
 
     @x.setter
     def x(self, value):
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        return (self.__y)
+        return self.__y
 
     @y.setter
     def y(self, value):
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
 
-
-def area(self):
-    """Calculates the area of the rectangle."""
-    return (self.width * self.height)
-
-    def display(self):
-        for _ in range(self.height):
-            print("#" * self.width)
+    def area(self):
+        return self.width * self.height
