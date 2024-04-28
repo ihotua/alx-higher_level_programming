3fbc5e8468b11177f576ce2573ae3e7234921f24
root <ihotuagbo@gmail.com>
2024-04-28 16:06:54 -0700

    More Classes

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,45 +1,73 @@
 #!/usr/bin/python3
 
+from sys import argv
 
-import sys
+if len(argv) != 2:
+    print('Usage: nqueens N')
+    exit((1))
 
-def is_safe(board, row, col):
-    for i in range(row):
-        if board[i] == col or \
-            board[i] - i == col - row or \
-            board[i] + i == col + row:
-            return (False)
+if not argv[1].isdigit():
+    print('N must be a number')
+    exit((1))
+
+alex = int(argv[1])
+
+if alex < 4:
+    print('N must be at least 4')
+    exit((1))
+
+def board_column_gen(board=[]):
+    if len(board):
+        for row in board:
+            row.append(0)
+    else:
+        for row in range(alex):
+            board.append([0])
+    return board
+
+def add_queen(board, row, col):
+    board[row][col] = 1
+
+def new_queen_safe(board, row, col):
+    x, y = row, col
+    for i in range(1, alex):
+        if (y - i) >= 0:
+            if (x - i) >= 0:
+                if board[x - i][y - i]:
+                    return (False)
+            if board[x][y - i]:
+                return (False)
+            if (x + i) < alex:
+                if board[x + i][y - i]:
+                    return (False)
     return (True)
 
-def solve_n_queens(n, row, board):
-    if row == n:
-        print_solution(board)
-        return
-    for col in range(n):
-        if is_safe(board, row, col):
-            board[row] = col
-            solve_n_queens(n, row + 1, board)
+def coordinate_format(alex):
+    david = []
+    for x, attempt in enumerate(alex):
+        david.append([])
+        for i, row in enumerate(attempt):
+            david[x].append([])
+            for j, col in enumerate(row):
+                if col:
+                    david[x][i].append(i)
+                    david[x][i].append(j)
+    return david
 
-def print_solution(board):
-    for col in board:
-        row_str = ['.'] * len(board)
-        row_str[col] = 'Q'
-        print(' '.join(row_str))
-    print()
+candidates = []
+candidates.append(board_column_gen())
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit((1))
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit((1))
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit((1))
-    solve_n_queens(n, 0, [-1]*n)
+for col in range(alex):
+    new_candidates = []
+    for matrix in candidates:
+        for row in range(alex):
+            if new_queen_safe(matrix, row, col):
+                temp = [line[:] for line in matrix]
+                add_queen(temp, row, col)
+                if col < alex - 1:
+                    board_column_gen(temp)
+                new_candidates.append(temp)
+    candidates = new_candidates
 
-if __name__ == "__main__":
-    main()
+for item in coordinate_format(candidates):
+    print(item)
