c0f5ce1c71bc3f741db90cc1c246571d681e1381
root <ihotuagbo@gmail.com>
2024-04-29 12:20:00 -0700

    Python IO

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,6 @@
 
 
 def read_file(filename=""):
+    """ function that reads a text file and prints it to stdout """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end='')
+        print(file.read(), end="")
