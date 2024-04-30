df63158bedbf9ecfcc6a73d54f1700bb50613e9b
root <ihotuagbo@gmail.com>
2024-04-30 13:59:55 -0700

    Python IO

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""")x)B. Python - Input/Output,Task O. Read file"""
+"""0x0B. Python - Input/Output,Task O. Read file"""
+
 
 def read_file(filename=""):
     """ function that reads a text file and prints it to stdout """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end="")
