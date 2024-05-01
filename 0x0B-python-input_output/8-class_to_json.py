b73a371b91309087f4c7a9455fe72b746fb8750a
root <ihotuagbo@gmail.com>
2024-05-01 14:58:02 -0700

    Python IO

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""0x0B. Python - Input/Output, task 8. Class to JSON"""
+
+
+def class_to_json(obj):
+    """ 
+    function that returns the dictionary description
+    """
+    return (obj.__dict__)
