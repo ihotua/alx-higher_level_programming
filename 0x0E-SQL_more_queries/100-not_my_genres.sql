2937c2aa52e6b3b8ae6ece3be77cb5b184fb6309
root <ihotuagbo@gmail.com>
2024-05-27 12:28:11 -0700

    SQL More

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -6,7 +6,7 @@ WHERE name
 NOT IN (SELECT y.name FROM tv_genres y
 	RIGHT JOIN tv_show_genres m
 	ON y.id = m.genre_id 
-	RIGHT JOIN tv_shows pc
+	RIGHT JOIN tv_shows p
 	ON m.show_id = p.id 
 	WHERE p.title = 'Dexter')
 ORDER BY 1 ASC;
