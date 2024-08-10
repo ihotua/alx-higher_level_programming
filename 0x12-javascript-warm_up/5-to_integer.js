09c1b6feff5b931a104faaceca4f53d244bb6a24
root <ihotuagbo@gmail.com>
2024-08-10 00:39:53 -0700

    JavaScript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* This script prints My number: <first argument converted in
  * integer> if the first argument can be converted to an integer
- */ 
+ */
 
 const args = process.argv.slice(2);
 const arg1 = parseInt(args[0], 10);
